6feef7f868235214b9b5b81e8d91a8ff259abd32
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-05-16 01:02:58 -0700

    Update index.js
    This changes the board setup into two loops, previously changed from hardcoded

--- starter-app/src/index.js
+++ starter-app/src/index.js
@@ -19,7 +19,7 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return (
-      <Square
+      <Square key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -27,24 +27,18 @@ class Board extends React.Component {
   }
 
   render() {
+      // Use two loops to make the squares
+      const boardSize = 3;
+      let squares = [];
+      for (let i=0; i < boardSize; ++i) {
+        let row = [];
+        for(let j=0; j < boardSize; ++j)  {
+          row.push(this.renderSquare(i * boardSize + j));
+        }
+        squares.push(<div className="board-row">{row}</div>);
+      }
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{squares}</div>
     );
   }
 }
